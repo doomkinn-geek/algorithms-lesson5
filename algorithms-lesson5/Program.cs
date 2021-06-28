a63377623cfcc5601f801346079a2ad71100ee91
doomkinn@yandex.ru <doomkinn@yandex.ru>
2021-06-28 19:12:36 +0300

    init

---
+++ algorithms-lesson5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace algorithms_lesson5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
